03ffa51a7f5e62e5cbbbdb57164d5fcfe32e4883
I1Leo <vanleo528@yandex.ru>
2024-01-03 04:19:00 +0300

    dev route

--- src/backend/modelpred/urls.py
+++ src/backend/modelpred/urls.py
@@ -3,6 +3,6 @@ from rest_framework import routers
 from modelpred import views
 
 router = routers.DefaultRouter()
-router.register('', views.PredictView, basename='predict')
+router.register('dev', views.PredictView, basename='predict')
 
 urlpatterns = router.urls
